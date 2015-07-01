7c5b43741e6c776ab72a543fb6c7dfc79ef6582b
bidit <biditpakrashi@gmail.com>
2015-07-01 07:37:57 +0530

    user service

---
+++ src/main/java/com/myorg/tools/documentworkflow/rest/resources/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.myorg.tools.documentworkflow.rest.resources.impl;
+
+import javax.ws.rs.core.Response;
+
+import com.myorg.tools.documentworkflow.dto.UserDTO;
+import com.myorg.tools.documentworkflow.model.DocumentDetail;
+import com.myorg.tools.documentworkflow.model.User;
+import com.myorg.tools.documentworkflow.rest.resources.UserService;
+
+public class UserServiceImpl implements UserService   {
+
+	@Override
+	public Response login(User user) {
+		// TODO Auto-generated method stub
+		System.out.println("user is " + user);
+		UserDTO  dto = new UserDTO();
+		dto.setUserid(1);
+		
+		return Response.ok().entity(dto).build();
+	}
+
+}
