5f9235255c60bbb1714f29c559d822d5ed9cd695
bidit <biditpakrashi@gmail.com>
2015-07-01 07:22:00 +0530

    user to

---
+++ src/main/java/com/myorg/tools/documentworkflow/model/User.java
@@ -0,0 +1,47 @@
+package com.myorg.tools.documentworkflow.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+	private String username;
+	
+	private String password;
+	
+	private String role;
+
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", role=" + role + "]";
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+
+}
