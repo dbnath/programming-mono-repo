54e74be28670e1b02065a5acf323f8ddb051a5d2
mreenal <mreenal@gmail.com>
2015-07-01 20:54:19 -0400

    home tab

--- WebContent/static/app/pages/home/homeCtrl.js
+++ WebContent/static/app/pages/home/homeCtrl.js
@@ -3,5 +3,13 @@ app.controller("homeCtrl",['service',function(service){
   var home = this;
   home.user = {};
   document.title = 'Docflow::Home';
+  
+  $('#myTabs a').click(function (e) {
+	  alert('Hi');
+	  e.preventDefault()
+	  $(this).tab('show')
+  })
 
 }]);
+
+
