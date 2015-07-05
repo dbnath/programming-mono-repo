e18f1e83f79e3982ebe1ac9cb9f58388f6b3e593
debn <User_2@NATHLAP>
2015-07-06 01:16:14 +0530

    commit for login module using cookie

--- src/main/java/com/myorg/tools/documentworkflow/config/DocumentWorkflowConfiguration.java
+++ src/main/java/com/myorg/tools/documentworkflow/config/DocumentWorkflowConfiguration.java
@@ -1,9 +1,15 @@
 package com.myorg.tools.documentworkflow.config;
 
 public class DocumentWorkflowConfiguration { 
-
+	private String userCookieHeaderName;
 	public void init() {
 		
-		
 	}
+	public String getUserCookieHeaderName() {
+		return userCookieHeaderName;
+	}
+	public void setUserCookieHeaderName(String userCookieHeaderName) {
+		this.userCookieHeaderName = userCookieHeaderName;
+	}
+	
 }
