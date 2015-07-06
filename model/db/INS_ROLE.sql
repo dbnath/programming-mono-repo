cabffea81b10a14452e8a7efc763bc68bdb93ac6
debn <debn@gmail.com>
2015-07-06 15:47:44 +0530

    dml scripts

---
+++ model/db/INS_ROLE.sql
@@ -0,0 +1,7 @@
+-- ROLE
+INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('1', 'Analyst', 'Analyst');
+INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('2', 'Attorney', 'Attorney');
+INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('3', 'QA', 'Lead Attorney');
+INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('4', 'Sys Admin', 'System Admin');
+
+
