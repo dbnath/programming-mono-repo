d2de5e89fcf6bdd8ac9c6ebc54c3025e4e56b29e
debn <debn@gmail.com>
2015-07-08 20:49:31 +0530

    ddl scripts as per apm's request

--- model/db/INS_ROLE.sql
+++ model/db/INS_ROLE.sql
@@ -1,4 +1,6 @@
 -- ROLE
+DELETE FROM ROLE;
+
 INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('1', 'Analyst', 'Analyst');
 INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('2', 'Attorney', 'Attorney');
 INSERT INTO ROLE (ID_ROLE, NM_ROLE, TX_ROLE) VALUES ('3', 'QA', 'Lead Attorney');
