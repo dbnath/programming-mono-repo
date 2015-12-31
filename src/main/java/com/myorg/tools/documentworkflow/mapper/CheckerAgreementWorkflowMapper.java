13e622e13aa44ab7c4878d863d5be08153c006e6
toolsrepo <toolsrepo@users.noreply.github.com>
2015-12-31 01:44:55 -0500

    Rashmi + Ardhendu + Pratik : All Rest calls committed

---
+++ src/main/java/com/myorg/tools/documentworkflow/mapper/CheckerAgreementWorkflowMapper.java
@@ -0,0 +1,34 @@
+package com.myorg.tools.documentworkflow.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.myorg.tools.documentworkflow.model.DocWkflwProcess;
+import com.myorg.tools.documentworkflow.util.DocumentWorkflowToolUtility;
+
+public class CheckerAgreementWorkflowMapper implements RowMapper<DocWkflwProcess> {
+	public DocWkflwProcess mapRow(ResultSet rs, int rowNum) throws SQLException {
+		DocWkflwProcess docWorkflow = new DocWkflwProcess();
+		if (!DocumentWorkflowToolUtility.isEmpty(rs)) {
+			docWorkflow.setAgreementId(rs.getInt("ID_AGRMT"));
+			docWorkflow.setAgreementTypeCode(rs.getInt("ID_AGREEMENT_TYPE"));
+			docWorkflow.setAgreementTypeDesc(rs.getString("TX_AGREEMENT_TYPE"));
+			docWorkflow.setRoleId(rs.getInt("ID_ROLE"));
+			docWorkflow.setStatusCode(rs.getInt("ID_WF_STATUS"));
+			docWorkflow.setStatusDescription(rs.getString("TX_WF_STATUS"));
+			docWorkflow.setMakerStatusId(rs.getInt("ID_WF_MAKER_STATUS"));
+			docWorkflow.setMakerStatus(rs.getString("TX_WF_MAKER_STATUS"));
+			docWorkflow.setMakerComments(rs.getString("MAKER_COM"));
+			docWorkflow.setLob(rs.getString("LOB"));
+		    docWorkflow.setAssignedTo(rs.getString("ASSIGNED_TO"));
+		    docWorkflow.setCreatedBy(rs.getString("CREATED_BY"));
+		    docWorkflow.setCreationDate(rs.getDate("CREATED_DT"));
+		    docWorkflow.setLastUpdatedBy(rs.getString("LAST_UPDATED_BY"));
+		    docWorkflow.setLastUpdationDate(rs.getDate("LAST_UPDATE_DT"));
+		}
+		return docWorkflow;
+    }
+
+}
